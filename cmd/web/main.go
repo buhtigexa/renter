b71fdf4aaf7d40581053676558575331e1a2f170
mrodriguez <mrodriguez@alumnos.exa.unicen.edu.ar>
2024-05-23 14:29:00 -0300

    first commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"github.com/buhtigexa/renter/pkg/models/mysql"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/buhtigexa/renter/pkg/models/postgresql"
+	_ "github.com/lib/pq" // Import the PostgreSQL driver
 	"log"
 	"net/http"
 	"os"
@@ -12,20 +12,20 @@ import (
 
 type Config struct {
 	Addr      string
-	StaticDic string
+	StaticDir string
 	dsn       string
 }
 
 type Application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
-	models   *mysql.ResourceModel
+	models   *postgresql.ResourceModel
 }
 
 func main() {
 	cfg := new(Config)
 	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
-	flag.StringVar(&cfg.dsn, "dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	flag.StringVar(&cfg.dsn, "dsn", "user=web password=pass dbname=snippetbox sslmode=disable", "PostgreSQL data source name")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
@@ -36,21 +36,21 @@ func main() {
 	}
 	defer db.Close()
 
-	app := &Application{errorLog: infoLog, infoLog: errorLog, models: &mysql.ResourceModel{DB: db}}
+	app := &Application{errorLog: errorLog, infoLog: infoLog, models: &postgresql.ResourceModel{DB: db}}
 
 	srv := &http.Server{
 		Addr:     cfg.Addr,
 		ErrorLog: errorLog,
-		Handler:  app.Routes(),
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", cfg.Addr)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
-
 }
+
 func openDB(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
+	db, err := sql.Open("postgres", dsn) // Change "mysql" to "postgres"
 	if err != nil {
 		return nil, err
 	}
